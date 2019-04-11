497ba09cd997d1d6d5611cbbfcf17a2310565ab6
Bart de Bever <bartdebever112@gmail.com>
2019-04-11 19:53:42 +0200

    BaseApiController with injected Context

---
+++ Template/Template.Api/Controllers/BaseApiController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Template.Data;
+
+namespace Template.Api.Controllers
+{
+    public abstract class BaseApiController : ControllerBase
+    {
+        protected ApplicationContext Context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseApiController"/> class.
+        /// </summary>
+        /// <param name="context">The injected context to be used.</param>
+        protected BaseApiController(DbContext context)
+        {
+            if (context is ApplicationContext appContext)
+            {
+                Context = appContext;
+            }
+        }
+    }
+}
