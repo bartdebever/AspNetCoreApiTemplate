2c7b1379e997e533654f17bc362b2a2c6977e777
Bart de Bever <bartdebever112@gmail.com>
2019-04-11 18:54:03 +0200

    Added all projects

---
+++ Template/Template.Models/Interfaces/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.Models.Interfaces
+{
+    /// <summary>
+    /// Interface to define database classes.
+    /// Can be expanded with data needed for every database table.
+    /// </summary>
+    public interface IEntity
+    {
+        /// <summary>
+        /// The id of the entity gathered from the database.
+        /// </summary>
+        long Id { get; set; }
+    }
+}
